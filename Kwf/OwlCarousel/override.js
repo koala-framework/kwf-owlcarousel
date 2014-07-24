ad0432b8b68269ef06f6683a5811b401e6e461c8
Niko Sams <ns@vivid-planet.com>
2014-07-24 17:22:35 +0200

    define dependencies and add override that uses modernizr
    needed so unittests can run in phantomjs

---
+++ Kwf/OwlCarousel/override.js
@@ -0,0 +1,5 @@
+var prevFn = $.fn.owlCarousel.Constructor.prototype.browserSupport;
+$.fn.owlCarousel.Constructor.prototype.browserSupport = function() {
+    prevFn.call(this);
+    this.support3d = Modernizr.csstransforms3d;
+};
