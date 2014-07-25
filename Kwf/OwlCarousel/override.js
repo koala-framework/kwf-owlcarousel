71f93d5a8f439c70cbbea928074b3a2c47d311ee
Niko Sams <ns@vivid-planet.com>
2014-07-25 09:09:30 +0200

    don't create global var

--- Kwf/OwlCarousel/override.js
+++ Kwf/OwlCarousel/override.js
@@ -1,5 +1,9 @@
+(function() {
+
 var prevFn = $.fn.owlCarousel.Constructor.prototype.browserSupport;
 $.fn.owlCarousel.Constructor.prototype.browserSupport = function() {
     prevFn.call(this);
     this.support3d = Modernizr.csstransforms3d;
 };
+
+})();
